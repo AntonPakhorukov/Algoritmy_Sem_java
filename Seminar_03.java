bb84e4b3d3f9b2bcd0d605b2b34ae6021d9a7932
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-05-29 12:47:32 +0700

    Seminar_03

---
+++ Seminar_03.java
@@ -0,0 +1,14 @@
+package Algoritmy_Semiar_01;
+
+public class Seminar_03 {
+
+
+
+    public class Node { //создаем ячейку
+        int value; // указывваем значение
+        Node next; // указывваем следующую ячейку
+
+    }
+
+    public class LinkedList
+}
